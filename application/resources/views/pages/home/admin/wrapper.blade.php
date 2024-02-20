5155c2cb80c5d37ee3c0fe28c9ee415d51c0b8de
Maryan Espinoza <analista.guma@gmail.com>
2024-02-20 16:38:32 -0600

    ✅ [ADD] Hide data in dashboard

--- application/resources/views/pages/home/admin/wrapper.blade.php
+++ application/resources/views/pages/home/admin/wrapper.blade.php
@@ -1,8 +1,5 @@
 <!--TOP STATS-->
 <div id="js-trigger-home-admin-wrapper">
-@include('pages.home.admin.widgets.first-row.wrapper')
-
-@include('pages.home.admin.widgets.second-row.wrapper')
 
 @include('pages.home.admin.widgets.third-row.wrapper')
 </div>
